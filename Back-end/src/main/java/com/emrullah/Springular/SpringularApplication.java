3138d6fab3997adaec9ac9f109256f361b6a4d07
Sangaibisi <emrullahyildirim@windowslive.com>
2019-05-18 11:35:02 +0300

    My first commit

---
+++ Back-end/src/main/java/com/emrullah/Springular/SpringularApplication.java
@@ -0,0 +1,13 @@
+package com.emrullah.Springular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringularApplication.class, args);
+	}
+
+}
