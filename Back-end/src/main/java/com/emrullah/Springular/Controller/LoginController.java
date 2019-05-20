a22da29711d6cea59b87d3a0cb44260b6c6ecbcd
Sangaibisi <emrullahyildirim@windowslive.com>
2019-05-20 23:16:26 +0300

    RestController has been updated for all CRUD operations for customer.

--- Back-end/src/main/java/com/emrullah/Springular/Controller/LoginController.java
+++ Back-end/src/main/java/com/emrullah/Springular/Controller/LoginController.java
@@ -4,6 +4,8 @@ import com.emrullah.Springular.Entitiy.Customer;
 import com.emrullah.Springular.Service.ICustomerService;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/customers")
 public class LoginController {
 
     ICustomerService _customerService;
@@ -19,9 +22,29 @@ public class LoginController {
     	this._customerService=customerService;
     }
     
-    @RequestMapping(value = "/customers", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public List<Customer> findAll(){
     	return _customerService.findAll();
     }
-
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+    public Customer findById(@PathVariable int id){
+    	return _customerService.findById(id);
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public void addCustomer(@RequestBody Customer customer){
+    	customer.setId(0);
+    	_customerService.save(customer);
+    }
+    
+    @RequestMapping(method = RequestMethod.PUT, produces = "application/json")
+    public void updateCustomer(@RequestBody Customer customer){
+    	_customerService.save(customer);
+    }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
+    public String deleteCustomerById(@PathVariable int id){
+    	return _customerService.deleteById(id);
+    }
 }
